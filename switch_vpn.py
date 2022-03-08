903344362ea8e2eb53373b248739c35843d44933
avery <avrypromo@pm.me>
2022-03-08 11:12:22 -0800

    documentation, better vars

--- switch_vpn.py
+++ switch_vpn.py
@@ -1,16 +1,21 @@
 import os
 
 
-def switch_vpn(vpn):
-    # fetch current wireguard configuration for next step:
-    current_vpn = os.popen("wg | head -n 1").read().rstrip().split()
+def switch_vpn(conf_filename):
+    # remove .conf suffix from wg config file to produce systemd service name
+    new_service = "wg-quick@" + conf_filename.rsplit('.', 1)[0]
 
-    # disable current wg interface if there is one
-    if current_vpn:
-        # wg output is "interface: conf_filename"
-        current_vpn = current_vpn[1]
-        os.system("systemctl disable --now wg-quick@" + current_vpn)
+    # fetch current wireguard systemd service for next step:
+    current_service = os.popen("wg | head -n 1").read().rstrip().split()
+    # wg output is "interface: conf_filename"
+    # so the split list is ["interface:",conf_filename]
 
 
-    # enable and start new wg interface from vpn
-    os.system("systemctl enable --now wg-quick@" + vpn.rsplit('.', 1)[0]) # remove .conf suffix
+    # disable current wg service if there is one
+    if current_service:
+        current_service = "wg-quick@" + current_service[1]
+        os.system("systemctl disable --now " + current_service)
+
+
+    # enable and start new wg service from vpn
+    os.system("systemctl enable --now " + new_service)
