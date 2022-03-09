a5da1f920585095a58d2667d4db58a4d2025751a
avery <avrypromo@pm.me>
2022-03-09 12:59:55 -0800

    alias support

--- pick.py
+++ pick.py
@@ -22,31 +22,43 @@ def main(args):
         vpn = vpns + args[0]
         args = args[1:]
     
+    # vpn is now e.g. "{path.dirname(__file__}/vpns/mullvad/"
     vpn += os.sep
     
     # now choose which vpn set to load.
     # e.g. every vpn from that provider minus 14-eyes nations, every european vpn, etc.
-    # TODO: polish this control flow
-    if args:
-        if not args[0].endswith('.json'):
-            args[0] += '.json'
-        if args[0] in os.listdir(vpn):
-            vpn += args[0]
-            args = args[1:]
-        else:
-            vpn += 'default.json'
+    # TODO support for "non14" instead of "non14.json"
+    if args and args[0] in os.listdir(vpn):
+        vpn += args[0]
+        args = args[1:]
     else:
         vpn += 'default.json'
 
+    # vpn is now the dict of location:[peers]
     with open(vpn) as f:
         vpn = json.load(f)
 
+    # if the location arg passed is a country code, or "united states" etc, the alias dict will hopefully handle it
+    with open(cfg + 'aliases.json') as f:
+        aliases = json.load(f)
+
+    # support for nested location dicts e.g. {america:{new york:{manhattan:[address1,address2]}, boston:[address1]}
     for location in args:
-        vpn = vpn[location]
+        if location in vpn:
+            vpn = vpn[location]
+        elif location in aliases:
+            vpn = vpn[aliases[location]]
+        else:
+            raise Exception(
+                f"{location} not understood. valid location args are:\n   {aliases.keys()}\n   {vpn.keys()}"
+                )
     
+    # at this point, if vpn is a host, there's nothing left to do
+    # if there are still nested dicts, randomly choose a value.
     while isinstance(vpn, dict):
         vpn = random.choice(list(vpn.values()))
 
+    # if vpn is a list, pick a random value
     if isinstance(vpn, list):
         vpn = random.choice(vpn)
 
