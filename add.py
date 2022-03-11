bbe9fea0c9a13bce9f090ff9b80a8eac7289b1e2
avery <avrypromo@pm.me>
2022-03-11 08:51:56 -0800

    theoretically functional

--- add.py
+++ add.py
@@ -1,3 +1,19 @@
-def create_vpn(vpn):
-    with open(confs + vpn, 'w+') as f:
-        json.dump({location:nodes for location,nodes in get_locations(vpn)})
+import os
+
+
+def create_vpn_profile(vpn, is_conf_member, location_from_conf, is_location_member=True,
+                        profile_name='default.json', confs_dir='/etc/wireguard'):
+
+    vpn_dir = os.path.dirname(__file__) + os.sep + vpn + os.sep
+    os.makedirs(vpn_dir)
+    profile_dict = {}
+
+    for conf in os.listdir(confs_dir): 
+        if is_conf_member(conf):
+            location = location_from_conf(conf)
+            if is_location_member(location):
+                location_list = profile_dict.setdefault(location,[])
+                location_list += conf
+
+    with open(vpn_dir + profile, 'w+') as f:
+        json.dump(profile_dict, f)
