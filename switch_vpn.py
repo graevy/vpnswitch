63a94978eb81d46c4dc29bd2b1f722ee26976f03
avery <avrypromo@pm.me>
2022-05-06 15:59:21 -0700

    streamlined some syntax

--- switch_vpn.py
+++ switch_vpn.py
@@ -5,17 +5,13 @@ def switch_vpn(conf_filename):
     # remove .conf suffix from wg config file to produce systemd service name
     new_service = "wg-quick@" + conf_filename.rsplit('.', 1)[0]
 
-    # fetch current wireguard systemd service for next step:
-    current_service = subprocess.run(['wg'], capture_output=True, text=True).stdout.split('\n', 1)[0]
-    # wg output is "interface: conf_filename"
-    # so the split list is ["interface:", conf_filename]
-
+    # fetch current wireguard systemd service
+    current_service = subprocess.run(['wg'], capture_output=True, text=True).stdout
 
     # disable current wg service if there is one
     if current_service:
-        current_service = "wg-quick@" + current_service[1]
+        current_service = "wg-quick@" + current_service.split(maxsplit=2)[1]
         subprocess.run(("systemctl disable --now " + current_service).split())
-
 
     # enable and start new wg service from vpn
     subprocess.run(("systemctl enable --now " + new_service).split())
